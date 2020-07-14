537dd9e669d3c383bfea8e678dd4eb1959fb012e
abhishekmg <abhishekmg.12@gmail.com>
2020-07-14 19:51:17 +0530

    first commit

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,13 @@
 module.exports = {
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'leap-blue': '#443EFF',
+        'leap-blue-1' : '#1D1A6E',
+        'leap-gray': '#F5F7F9',
+        'leap-gray-1' : '#888897'
+      }
+    },
   },
   variants: {},
   plugins: [],
