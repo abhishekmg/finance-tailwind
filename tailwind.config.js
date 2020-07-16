56798528ad64840b76d6954bee85d691eaa966ea
abhishekmg <abhishekmg.12@gmail.com>
2020-07-16 14:45:07 +0530

    done till section 3

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,8 +4,11 @@ module.exports = {
       colors: {
         'leap-blue': '#443EFF',
         'leap-blue-1' : '#1D1A6E',
+        'leap-blue-2' : '#1CA0F2',
         'leap-gray': '#F5F7F9',
-        'leap-gray-1' : '#888897'
+        'leap-gray-1' : '#888897',
+        'leap-gray-2' : '#565660',
+        'leap-black' : '#06051F'
       }
     },
   },
