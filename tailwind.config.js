d0d3e28f4a42303dfacffe39c7fbceeab5430bf4
abhishekmg <abhishekmg.12@gmail.com>
2020-07-23 13:51:16 +0530

    fixed table res

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ module.exports = {
         'leap-blue': '#443EFF',
         'leap-blue-1' : '#1D1A6E',
         'leap-blue-2' : '#1CA0F2',
+        'leap-blue-3' : '#817DFF;',
         'leap-gray': '#F5F7F9',
         'leap-gray-1' : '#888897',
         'leap-gray-2' : '#565660',
